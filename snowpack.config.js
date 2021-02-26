b481d0b5db4227c736d4a3174e113e3d5a66ff7e
saunter511 <b.barcicki1999@gmail.com>
2021-02-26 22:48:36 +0100

    Fix snowpack build

--- snowpack.config.js
+++ snowpack.config.js
@@ -23,11 +23,9 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    buildOptions: {
       // put the build files in /docs
       out: 'docs',
       // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
       metaUrlPath: 'snowpack'
-    }
   },
 };
